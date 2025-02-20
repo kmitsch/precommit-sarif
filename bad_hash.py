ec4d73c8d39b2dac3e3e5a4fadbb4ee4a1a2f0f8
Kevin Mitsch <kmitsch@gmail.com>
2025-02-20 08:43:49 +0100

    feat: test report uploap, report generation take three

--- bad_hash.py
+++ bad_hash.py
@@ -3,6 +3,7 @@ import hashlib
 # Hardcoded secret (Insecure: Should be stored securely)
 SECRET_KEY = "hardcoded_secret_12345678"
 
+
 # Use of MD5 for hashing (Insecure: Use SHA-256 or stronger instead)
 def insecure_hash(password):
     return hashlib.md5(password.encode()).hexdigest()
